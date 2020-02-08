3ede5d8b7e64a882933a3ff1b05eb1d553325a0b
tcantlay <tannercantlay@gmail.com>
2020-02-08 13:45:58 -0500

    Scoreboard added

---
+++ Pong/p1Score.gd
@@ -0,0 +1,18 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var manager = get_node("../../OuterRing")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var text = manager.p1Name + "\nScore: " + str(manager.p1Score)
+	set_text(text)
+	pass
