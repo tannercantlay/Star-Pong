a076864c276c1b2da3a56a628f454f821aeaac24
Terragos <47040288+Terragos@users.noreply.github.com>
2020-02-08 09:14:40 -0500

    Updated Ball Bounce

--- Pong/ball.gd
+++ Pong/ball.gd
@@ -4,16 +4,29 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var velocity
+var velocity = Vector2()
+var returnValue
+var speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = Vector2(10,10)
+	velocity = Vector2(speed,speed)
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	move_and_collide(velocity)
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+		if collision.collider.has_method("hit"):
+			print("Entered Hit Method")
+			collision.colldier.hit()
+
+	#returnValue = move_and_collide(velocity)
+	
+	#if(returnValue != null):		
+		#print(returnValue.get_collider())
+	
 	pass
