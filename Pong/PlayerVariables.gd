94d578dcdf9400e5d4e4933a69280c49be88fcf8
tcantlay <tannercantlay@gmail.com>
2020-02-08 14:46:06 -0500

    added rounds played counter

--- Pong/PlayerVariables.gd
+++ Pong/PlayerVariables.gd
@@ -8,6 +8,7 @@ var player1 = "Player 1"
 var player2 = "Player 2"
 var p1Score = 0
 var p2Score = 0
+var numrounds = 11
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Pong/gameManager.gd
+++ Pong/gameManager.gd
@@ -2,11 +2,13 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var roundsPlayed = 1
 var velocity = Vector2(0,0)
 onready var player_vars = get_node("/root/PlayerVariables")
+onready var roundLabel = get_node("../Node/RoundsPlayed")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	roundLabel.set_text("Round: " + str(roundsPlayed))
 	pass # Replace with function body.
 
 func hit(team):
@@ -18,9 +20,14 @@ func hit(team):
 #			collision.collider.hit()
 	if(team == "purple"):
 		player_vars.p1Score += 1
+		player_vars.numrounds -= 1
 	if(team == "yellow"):
 		player_vars.p2Score += 1
+		player_vars.numrounds -= 1
+	roundsPlayed += 1
+	roundLabel.set_text("Round: " + str(roundsPlayed))
 	pass
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
