ed36ecc5b34540948b80fad085bee98296e4b94d
tcantlay <tannercantlay@gmail.com>
2020-02-08 14:28:21 -0500

    added global player variables

--- Pong/gameManager.gd
+++ Pong/gameManager.gd
@@ -1,15 +1,12 @@
 extends KinematicBody2D
-var p1Name = "Player 1"
-var p2Name = "Player 2"
-var p1Score = 0
-var p2Score = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var velocity = Vector2(0,0)
-
+onready var player_vars = get_node("/root/PlayerVariables")
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 func hit(team):
@@ -20,9 +17,9 @@ func hit(team):
 #			print_debug("ball Entered Hit Method")
 #			collision.collider.hit()
 	if(team == "purple"):
-		p1Score += 1
+		player_vars.p1Score += 1
 	if(team == "yellow"):
-		p2Score += 1
+		player_vars.p2Score += 1
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
