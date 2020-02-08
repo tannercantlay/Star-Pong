6585c929fd9d1080f98a19142b3f9b70bb475c5e
tcantlay <tannercantlay@gmail.com>
2020-02-08 00:16:46 -0500

    Rotation fixed for paddle

--- Pong/paddle.gd
+++ Pong/paddle.gd
@@ -5,15 +5,15 @@ extends KinematicBody2D
 # var b = "text"
 
 var theta
-var locationX
-var locationY
+var locationX = 0
+var locationY = -1
 var location
-var radius = 50
+var radius = 22.5
 #var teamColor = player.color or whatever
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	theta = 3 * PI/4
+	theta = PI
 	pass
 #	if teamColor == "purple":
 #		theta = PI/2
@@ -24,10 +24,14 @@ func _input(event):
 	if event is InputEventKey and event.pressed:
 		#if statement to determine what teamColor we are
 		if event.scancode == KEY_A:
-			theta += .1
+			theta += .05
+			locationX =  cos(theta)
+			locationY = sin(theta)
 			translate(location)
 		elif event.scancode == KEY_D:
-			theta -= .1
+			theta -= .05
+			locationX =  -cos(theta)
+			locationY = -sin(theta)
 			translate(location)
 	pass
 
@@ -40,9 +44,9 @@ func _integrate_forces(state):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	locationX = radius * cos(theta)
-	locationY = radius * sin(theta)
 	
-	location = Vector2(locationX, locationY)
+	
+	location = radius * Vector2(locationX, locationY)
+	print_debug(location)
 	
 	pass
