777755eb3ed818c8b5f1e1b2f0ee4a3f30911471
Jameson Hale <halejameson@gmail.com>
2020-02-08 17:00:34 -0500

    added momentum for speedup on paddles

--- Pong/paddle1.gd
+++ Pong/paddle1.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-var theta
+var theta = 0
 var locationX = 0
 var locationY = -1
 var location
@@ -17,6 +17,7 @@ var team = "purple"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rotation_dir = 0
+	set_process_input(true)
 	pass
 func getTeam():
 	return team
@@ -26,13 +27,28 @@ func reset():
 	rotation = 0
 	rotation_dir = 0
 	pass
+	
+func _input(event):
+	if event is InputEventKey and ((event.get_scancode() == 68 and event.is_pressed() == false) or (event.get_scancode() == 65 and event.is_pressed() == false)):
+		theta = 0
+		#while (abs(theta) > 0.0 && !Input.is_key_pressed(65) && !Input.is_key_pressed(68)):
+			#print_debug("in while loop")
+			#(theta -= 0.005)
+			#rotation_dir -= theta
+		
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if(Input.is_key_pressed(65)): #Key_A
-		rotation_dir += .1
+		if (abs(theta) < 0.1):
+			(theta += 0.006)
+		rotation_dir += theta
 	if(Input.is_key_pressed(68)): #Key_D
-		rotation_dir -= .1
+		if (abs(theta) < 0.1):
+			(theta += 0.006)
+		rotation_dir -= theta
+		
 		
 	rotation = rotation_dir * rotation_speed * delta
 	pass
