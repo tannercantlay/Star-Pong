8c99a7be7eb2dbd63c8ba39a582f2b96aa2287cc
Terragos <47040288+Terragos@users.noreply.github.com>
2020-02-09 22:52:06 -0500

    BugFixes

--- Pong/gameManager.gd
+++ Pong/gameManager.gd
@@ -23,7 +23,6 @@ func _ready():
 	musicPlayer.music.volume_db = 1
 	musicPlayer.music.pitch_scale = 1
 	musicPlayer.music.play()
-	pass # Replace with function body.
 
 func hit(team):
 	if(team == "purple"):
@@ -81,10 +80,10 @@ func _physics_process(delta):
 			get_node("..").add_child(powerup)
 			var wormhole1 = get_node("../Wormholes/Wormhole1")
 			var wormhole2 = get_node("../Wormholes/Wormhole2")
-			var temp = Vector2(rand_range(-25, -225), rand_range(-225,225))
+			var temp = Vector2(rand_range(-30, -225), rand_range(-225,225))
 			#print_debug(temp)
 			wormhole1.position = temp
-			temp = Vector2(rand_range(25, 225), rand_range(-225,225))
+			temp = Vector2(rand_range(30, 225), rand_range(-225,225))
 			#print_debug(temp)
 			wormhole2.position = temp
 		
