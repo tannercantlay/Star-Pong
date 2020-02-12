8cb7c22995fd9090784f3bc5093c0994a7542e9e
tcantlay <tannercantlay@gmail.com>
2020-02-12 13:12:37 -0500

    Added theme change in the settings menu

---
+++ Pong/BlueRedTheme.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var player_vars = get_node("/root/PlayerVariables")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == BUTTON_LEFT:
+		
+		if get_rect().has_point(get_local_mouse_position()):
+				player_vars.p1RingColor = load("res://Sprites/SpriteSheets/OuterRingBlueToRed.png")
+				player_vars.p2RingColor = load("res://Sprites/SpriteSheets/OuterRingRedToBlue.png")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Pong/PurpleYellowTheme.gd
+++ Pong/PurpleYellowTheme.gd
@@ -8,13 +8,13 @@ extends Sprite
 onready var player_vars = get_node("/root/PlayerVariables")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if(get_tree().get_current_scene().get_name() == "Name1"):
-		texture = player_vars.p1RingColor
-	elif(get_tree().get_current_scene().get_name() == "Name2"):
-		texture = player_vars.p2RingColor
 	pass # Replace with function body.
 
-
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == BUTTON_LEFT:
+		if get_rect().has_point(get_local_mouse_position()):
+				player_vars.p1RingColor = load("res://Sprites/SpriteSheets/OuterRingYtoP.png")
+				player_vars.p2RingColor = load("res://Sprites/SpriteSheets/OuterRingPtoY.png")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
