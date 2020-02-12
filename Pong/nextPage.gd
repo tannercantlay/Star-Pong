efee06a0f186279b49697a522dd7df6f3aaf48b5
Terragos <47040288+Terragos@users.noreply.github.com>
2020-02-12 17:42:51 -0500

    More Changes lol
    1) Updated the new sprites
    2) Fixed Enter key Bug

--- Pong/nextPage.gd
+++ Pong/nextPage.gd
@@ -45,11 +45,36 @@ func _ready():
 			paddle2.texture = player_vars.p2PaddleColor
 			
 func _input(event):
-	if event is InputEventKey and ((event.get_scancode() == 16777217 and event.is_pressed() == false)): 
+	if event is InputEventKey and (event.get_scancode() == 16777217 and event.is_pressed() == false): 
 		get_tree().quit()
+	if event is InputEventKey and (event.get_scancode() == 16777221 and event.is_pressed() == false):
+		if(get_tree().get_current_scene().get_name() == "Name1"):
+			get_tree().change_scene("res://Scenes/EnterName2.tscn")
+		elif(get_tree().get_current_scene().get_name() == "Name2"):
+			get_tree().change_scene("res://Scenes/OuterCollider.tscn")
+		elif(get_tree().get_current_scene().get_name() == "main"):
+			player_vars.p1Score = 0
+			player_vars.p2Score = 0
+			player_vars.numrounds = 11
+			player_vars.maxwins = player_vars.numrounds / 2 + 1 #All that matters
+			player_vars.player1 = "Player 1"
+			player_vars.player2 = "Player 2"
+			player_vars.winner = player_vars.player1
+			get_tree().change_scene("res://Scenes/EnterName1.tscn")
+		elif(get_tree().get_current_scene().get_name() == "gamewin"):
+			musicPlayer.music.stop()
+			musicPlayer.stream = load("res://Sounds/mainmenu.ogg")
+			musicPlayer.music.set_stream(musicPlayer.stream)
+			musicPlayer.music.volume_db = 1
+			musicPlayer.music.pitch_scale = 1
+			musicPlayer.music.play()
+			get_tree().change_scene("res://Scenes/MainMenu.tscn")
+		else:
+			print_debug("1st: " + get_tree().get_current_scene().get_name())
+			print_debug("2nd: " + "EnterName1")
 				
 func _unhandled_input(event):
-	if (event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == BUTTON_LEFT) || (event is InputEventKey and ((event.get_scancode() == 16777221 and event.is_pressed() == false))):
+	if (event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == BUTTON_LEFT):
 		if get_rect().has_point(get_local_mouse_position()):
 			if(get_tree().get_current_scene().get_name() == "Name1"):
 				get_tree().change_scene("res://Scenes/EnterName2.tscn")
@@ -58,7 +83,7 @@ func _unhandled_input(event):
 			elif(get_tree().get_current_scene().get_name() == "main"):
 				player_vars.p1Score = 0
 				player_vars.p2Score = 0
-				player_vars.numrounds = 1
+				player_vars.numrounds = 11
 				player_vars.maxwins = player_vars.numrounds / 2 + 1 #All that matters
 				player_vars.player1 = "Player 1"
 				player_vars.player2 = "Player 2"
