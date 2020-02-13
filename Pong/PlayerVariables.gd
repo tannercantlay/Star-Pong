de74fc25fa2139b1d77d7aad141a2be032f157f9
Terragos <47040288+Terragos@users.noreply.github.com>
2020-02-13 17:08:12 -0500

    Bugfixes
    1) Fixed a bug that made certain color pallets only work on my machine.
    2) Renamed the YtoP to YellowToPurple.

--- Pong/PlayerVariables.gd
+++ Pong/PlayerVariables.gd
@@ -11,8 +11,8 @@ var p2Score
 var numrounds = null
 var winner
 var maxwins
-var p1RingColor = load("res://Sprites/SpriteSheets/OuterRingYToP.png")
-var p2RingColor = load("res://Sprites/SpriteSheets/OuterRingPToY.png")
+var p1RingColor = load("res://Sprites/SpriteSheets/OuterRingYellowToPurple.png")
+var p2RingColor = load("res://Sprites/SpriteSheets/OuterRingPurpleToYellow.png")
 var p1PaddleColor = load("res://Sprites/SpriteSheets/Dope_Purple_Paddle.png")
 var p2PaddleColor = load("res://Sprites/SpriteSheets/Dope_Yellow_Paddle.png")
 var player1Color = "purple"
--- Pong/ThemePurpleYellow.gd
+++ Pong/ThemePurpleYellow.gd
@@ -5,8 +5,8 @@ onready var player_vars = get_node("/root/PlayerVariables")
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(get_local_mouse_position()):
-			player_vars.p1RingColor = load("res://Sprites/SpriteSheets/OuterRingYtoP.png")
-			player_vars.p2RingColor = load("res://Sprites/SpriteSheets/OuterRingPtoY.png")
+			player_vars.p1RingColor = load("res://Sprites/SpriteSheets/OuterRingYellowToPurple.png")
+			player_vars.p2RingColor = load("res://Sprites/SpriteSheets/OuterRingPurpleToYellow.png")
 			player_vars.player1Color = "purple"
 			player_vars.player2Color = "yellow"
 			player_vars.p1PaddleColor = load("res://Sprites/SpriteSheets/Dope_Purple_Paddle.png")
