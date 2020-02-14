ac47fde4a6416c9757930facd7a9032288316310
Terragos <47040288+Terragos@users.noreply.github.com>
2020-02-13 23:07:54 -0500

    Bugfix
    1) Changes an even number to that number +1 in the lineedit for numRounds.

--- Pong/getNumRounds.gd
+++ Pong/getNumRounds.gd
@@ -4,7 +4,14 @@ onready var player_vars = get_node("/root/PlayerVariables")
 onready var lineedit = get_node(".")
 
 func _physics_process(delta):	
-	if(get_tree().get_current_scene().get_name() == "settings"):
-		if(lineedit.get_text().length() >= 1 ):
-			player_vars.numrounds = int(get_text())
+	if(get_tree().get_current_scene().get_name() == "settings"):	
+		var stringInput = lineedit.get_text()
+		
+		if(stringInput.length() >= 1 ):
+			var intInput = int(stringInput)
+			
+			if intInput % 2 == 0:
+				intInput = intInput + 1
+				
+			player_vars.numrounds = intInput
 			player_vars.maxwins = player_vars.numrounds / 2 + 1
